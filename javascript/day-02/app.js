337164d55791ce489b20439dc04f0fef6f6a12e2
enggahmedfarooq <ahmedfarooqgcp@gmail.com>
2026-07-16 23:48:37 +0500

    app

---
+++ javascript/day-02/app.js
@@ -0,0 +1,15 @@
+// Practing If statement:
+
+let color = "green";
+
+if(color === "red"){
+    console.log("Stop Traffic Light is Red");
+}
+
+if(color === "yellow"){
+    console.log("Slow down traffic light is yellow");
+}
+
+if(color === "green"){
+    console.log("Go go your way is clear");
+}
