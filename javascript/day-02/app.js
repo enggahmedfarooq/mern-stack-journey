289a5a8effc4fff3a5dc56a1b0e22ea539eb34ad
enggahmedfarooq <ahmedfarooqgcp@gmail.com>
2026-07-24 22:26:20 +0500

    day-02 pracrice

--- javascript/day-02/app.js
+++ javascript/day-02/app.js
@@ -1,5 +1,5 @@
+/*
 // Practing If statement:
-
 let color = "green";
 
 if(color === "red"){
@@ -13,5 +13,63 @@ if(color === "yellow"){
 if(color === "green"){
     console.log("Go go your way is clear");
 }
+*/
 
-//prac
+
+/*
+//Quesion#1
+
+let num = 101;
+if(num % 10 == 0)
+{
+    console.log("Good");
+}
+else 
+{
+    console.log("Bad");
+}
+*/
+
+/*
+//Question#2
+let name = prompt("Enter your name :");
+let age = prompt("Enter your age :");
+alert(`${name} is ${age} year old`);
+*/
+
+
+/*
+//Question#3
+let months = "4";
+switch (months) {
+    case "1" :
+        console.log("January — February — March");
+         break;
+    case "2" :
+        console.log("April — May — June");
+         break;
+    case "3" :
+        console.log("July — August — September");
+         break;
+    case "4" :
+        console.log(" October — November — December ");
+         break;
+    default :
+    console.log("Invalid Value");
+}
+*/
+
+/*
+//Question#4
+let str = "ahmedfarooq";
+
+if( (str[0] == 'A' || str[0] == 'a') && (str.length > 5) )
+{
+    console.log("GOlden String");
+}
+
+else{
+    console.log("not");
+}
+
+*/
