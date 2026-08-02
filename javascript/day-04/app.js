accb3e0ed2addbe25f581b92ed0704b58c67323a
enggahmedfarooq <ahmedfarooqgcp@gmail.com>
2026-08-02 22:25:37 +0500

    app.js

---
+++ javascript/day-04/app.js
@@ -0,0 +1,17 @@
+/*
+//FAVOURITE MOVIE GUESSING GAME:
+const favMovie = "marvel";
+
+let guess = prompt("Guess my favourite movie:");
+
+while ((guess != favMovie) && (guess != "quit")) {
+    console.log("Wrong");
+    guess = prompt("Try again to guess the movie:");
+}
+
+if (guess == favMovie) {
+    console.log("Congragulations");
+} else {
+    console.log("You quite");
+}
+ */
