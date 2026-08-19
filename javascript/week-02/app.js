038c45ebad50d7ff4e29da056960aa7313f46725
enggahmedfarooq <ahmedfarooqgcp@gmail.com>
2026-08-19 23:16:30 +0500

    Practicing Obj

---
+++ javascript/week-02/app.js
@@ -0,0 +1,24 @@
+/*
+//the below progarm is used to generate the random number from 1 to 6 every time;
+let num = Math.random();
+num = Math.random()*6;
+num = Math.floor(num);
+num = num + 1;
+*/
+
+const car = {
+    name : "Toyota",
+    model : 1998,
+    price : 10000
+
+};
+console.log(car.name);
+
+const person = {
+ name : "Ahmed",
+ age : 19,
+ country : "Peshawar"
+};
+
+person.country = "Pakistan";
+person.gender = "male";
